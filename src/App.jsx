ed842482d125dcdb5de2f4460a66165f37ceecbe
Andres Guerrero <aguerrero@fi.uba.ar>
2024-08-03 09:23:50 -0300

    add counterSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { increment , decrement , incrementBy } from './store/slices/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector( state => state.counter )
+
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -16,10 +21,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is { counter }</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={ ()=>  dispatch( increment() ) }>
+          Increment
+        </button>
+        <button onClick={ ()=>  dispatch( decrement() ) }>
+          Decrement
+        </button>
+        <button onClick={ ()=>  dispatch( incrementBy(2) ) }>
+          Increment by 2
         </button>
       </div>
     </>
